40be0a54a9238b013f0ab05c1464a327644074f7
JorgeGranda72 <73796286+JorgeGranda72@users.noreply.github.com>
2020-12-25 16:15:56 -0500

    Update countOfAllBooleans.js

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,4 +1,10 @@
 function countOfAllBooleans(arr) {
+  var count = 0;
+  for(var i = 0;i < array.length; i++){
+    if(array[i] == true || array[i] == false)
+    count++
+}
+  return count;
   // Insert code here;
 }
 
