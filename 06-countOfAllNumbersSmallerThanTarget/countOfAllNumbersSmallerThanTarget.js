0169ae25138aafe1fb1c08cd7c904dc063583088
JorgeGranda72 <73796286+JorgeGranda72@users.noreply.github.com>
2020-12-25 18:06:13 -0500

    Update countOfAllNumbersSmallerThanTarget.js

--- 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
+++ 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
@@ -1,4 +1,9 @@
 function countOfAllNumbersSmallerThanTarget(nums, target) {
+  var count = 0;
+  for(var i = 0; i< nums.length; i++)
+    if(nums[i]<target)
+        count ++;
+  return count;
   // Insert code here;
 }
 
