65128d8e025a61eae5f030088296c2d70276074b
JorgeGranda72 <73796286+JorgeGranda72@users.noreply.github.com>
2020-12-26 12:48:50 -0500

    Update sumOfNumsWithinARange.js

--- 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
+++ 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
@@ -1,7 +1,7 @@
 function sumOfNumsWithinARange(nums, start, end) {
   var rangeCount = 0;
   for(var i = 0; i < nums.length; i++){
-    if(nums[i] >= star && nums[i] <= end)
+    if(nums[i] >= start && nums[i] <= end)
       rangeCount ++;
     }
   return rangeCount;
