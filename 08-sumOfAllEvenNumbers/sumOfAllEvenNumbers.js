083474f0bc4ee5be58c6b2d05abea6e19bfadb44
JorgeGranda72 <73796286+JorgeGranda72@users.noreply.github.com>
2020-12-25 19:16:35 -0500

    Update sumOfAllEvenNumbers.js

--- 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
+++ 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
@@ -1,4 +1,10 @@
 function sumOfAllEvenNumbers(nums) {
+  var count = 0;
+  for(var i = 0; i < nums.length; i++){
+    if(nums[i]%2==0)
+      count ++;
+    }
+  return count;
   // Insert code here;
 }
 
