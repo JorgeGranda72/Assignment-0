10ac53d0447f42e90c5b7286391b395503a91882
Jorge Granda <jorge.granda@jjay.cuny.edu>
2020-12-28 22:07:05 -0500

    Adding solution to problem

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,5 +1,14 @@
 function pairSum(nums, target) {
-  // Insert code here;
+  if(nums.length <= 1) throw "Length of `nums` array is 1 or lower";
+  //Checks the combinations through each element, finds value that equals target     
+  for(var i = 0; i < nums.length; i++){
+    for(var j = 1; j < nums.length; j++){
+      if(nums[i] + nums[j] == target){
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 // Do not edit this line;
