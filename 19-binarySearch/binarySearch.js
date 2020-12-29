0b3acba73a3b0767ff7cfa97fd024fcd28c13c39
Jorge Granda <jorge.granda@jjay.cuny.edu>
2020-12-29 00:15:01 -0500

    Adding solution to problem

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -3,9 +3,31 @@ class MySolution {
     this.flag = false; // this is optional to use;
   }
 
-  binarySearch(nums, target) {
-    // Insert code here;
+  binarySearch(nums, target){
+    let start = 0;
+    let end = nums.length - 1;
+    let mid; // Middle value
+    let flag = false;
+    
+    
+    while(!flag && start <= end){
+      mid = Math.floor((start + end)/2);
+      //Finds target
+      if(nums[mid] === target){ 
+        flag = true;
+      }
+      //Target on the right side 
+      else if(nums[mid] < target){ 
+        start = mid + 1;
+      } 
+      //Target on the left side
+      else{ 
+        end = mid - 1;
+      }
+    }
+    return flag;
   }
+  
 }
 
 // Do not edit this line;
